53e2903560fe0aad8e932d2b406ae9b974531ec3
dizmanaga <dizmanaga@gmail.com>
2023-10-06 12:28:53 +0300

    add class dropdown

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -4,6 +4,8 @@
     'items' => null,
     'color' => 'primary',
     'size' => null,
+    'dropdown_class' => null,
+    'dropdown_menu_class' => null,
 ])
 
 @php
@@ -11,21 +13,21 @@
         'btn btn-' . $color,
         'btn-' . $size => $size,
         'dropdown-toggle',
-        'border-0 p-0' => $color == 'link',
+        'border-0 p-0' => $color == 'link'
     ])->merge([
         'type' => 'button',
         'data-bs-toggle' => 'dropdown',
     ]);
 @endphp
 
-<div class="dropdown d-inline-block">
+<div class="dropdown d-inline-block {{ $dropdown_class }}">
     <button {{ $attributes }}>
         <x-bs::icon :name="$icon"/>
 
         {{ $label }}
     </button>
 
-    <div class="dropdown-menu">
+    <div class="dropdown-menu {{ $dropdown_menu_class }}">
         {{ $items ?? $slot }}
     </div>
 </div>
